a470d08dbcc02ed7b95ed9867f82ecb47497e7ba
jaehafe <wogkdkrm112@gmail.com>
2024-08-06 15:53:29 +0900

    Initial Commit

---
+++ swift-start/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-start
+//
+//  Created by jaeha on 8/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-start/swift_startApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_startApp.swift
+//  swift-start
+//
+//  Created by jaeha on 8/6/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_startApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
